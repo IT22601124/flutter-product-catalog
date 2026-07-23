edf6f81ccc6cba86d8f724414e6e0fed7331e84e
Tharindu <it22601124@my.sliit.lk>
2026-07-23 11:45:51 +0530

    refactor: add central AppConstants for configuration

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String baseUrl = 'https://fakestoreapi.com';
+  static const String favoritePrefKey = 'favorite_product_ids';
+  static const String themePrefKey = 'is_dark_mode';
+}
